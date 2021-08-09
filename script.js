cbe80a4e802efb3db0c32bcd0a2cd3f63c110019
Kirill Lavrov <lkv@myttk.ru>
2021-08-09 18:40:10 +0700

    feat (script.js): Additional task completed

--- script.js
+++ script.js
@@ -1,52 +1,72 @@
 'use strict';
+
 const
   launchTheGame = function() {
-    const
+    let
 // Границы загадываемых чисел
-    min = 1,
-    max = 100,
+      min = 1,
+      max = 100,
+// Количество попыток
+      numberOfAttempts = 10,
 // Генератор псевдослучайного число в заданных границах
-    getRandomIntInclusive = function() {
-      return Math.floor(Math.random() * (max - min + 1)) + min;
-    },
+      getRandomIntInclusive = function() {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+      },
 // Псевдослучайное число которое предстоит угадать
-    hiddenNumber = getRandomIntInclusive(),
+      hiddenNumber = getRandomIntInclusive(),
 // Функция запроса ввода значений у пользователя
-    enterTheNumber = function(lastTry) {
-      let userResponse;
-      switch (true) {
-        case lastTry === undefined:
-          userResponse = prompt(`Угадай число от ${min} до ${max}`); break;
-        case isNaN(lastTry) || (+lastTry < min || +lastTry > max):
-          userResponse = prompt(`Введи число! (от ${min} до ${max})`); break;
-        case lastTry < hiddenNumber: 
-          userResponse = prompt('Загаданное число больше'); break;
-        case lastTry > hiddenNumber: 
-          userResponse = prompt('Загаданное число меньше'); break;
-      }
-      switch (true) {
-        case userResponse === null:
-          return false;
-        case isNaN(userResponse) || (+userResponse < 1 || +userResponse > 100): 
-          return enterTheNumber(userResponse);
-        default:
-          return userResponse;
-      } 
-    },
-//  Функция хода в игре
-    guessTheNumber = function(lastTry) {
-      let userNumber = enterTheNumber(lastTry);
-
-      if (userNumber) {
-        if (hiddenNumber === +userNumber) {
-          alert('Поздравляю, Вы угадали!!!');
-        } else {
-          guessTheNumber(userNumber);
+      enterTheNumber = function(lastTry, attemptСounter) {
+        let userResponse;
+        switch (true) {
+          case lastTry === undefined:
+            userResponse = prompt(`Угадай число от ${min} до ${max}`); break;
+          case isNaN(lastTry) || (+lastTry < min || +lastTry > max):
+            userResponse = prompt(`Введи число! (от ${min} до ${max})`); break;
+          case lastTry < hiddenNumber: 
+            userResponse = prompt(`Загаданное число больше, осталось попыток ${
+              attemptСounter ? numberOfAttempts - attemptСounter : numberOfAttempts}`); break;
+          case lastTry > hiddenNumber: 
+            userResponse = prompt(`Загаданное число меньше, осталось попыток ${
+              attemptСounter ? numberOfAttempts - attemptСounter : numberOfAttempts}`); break;
         }
-      } else {
-        alert('Игра окончена');
-      }
-    };
+        switch (true) {
+          case userResponse === null:
+            return false;
+          case isNaN(userResponse) || (+userResponse < 1 || +userResponse > 100): 
+            return enterTheNumber(userResponse);
+          default:
+            return userResponse;
+        } 
+      },
+//  Функция хода в игре
+      guessTheNumber = function(lastTry, attemptСounter) {
+        let userNumber = enterTheNumber(lastTry, attemptСounter),
+//  Диалог перезапуска игры
+        restartGame = function(userWon) {
+          if(confirm(userWon ? 
+            'Поздравляю, Вы угадали!!! Хотели бы сыграть еще?':
+            'Попытки закончились, хотите сыграть еще?')) {
+              hiddenNumber = getRandomIntInclusive();
+              guessTheNumber();
+            } else {
+            alert('Благодарим за игру');
+          }
+        };
+
+        if (userNumber) {
+          if (hiddenNumber === +userNumber) {
+            restartGame(true);
+          } else {
+            if (attemptСounter === numberOfAttempts - 1) {
+              restartGame(false);
+            } else {
+              guessTheNumber(userNumber, attemptСounter ? ++attemptСounter : 1);
+            }
+          }
+        } else {
+          alert('Игра окончена');
+        }
+      };
     guessTheNumber();
   };
 
