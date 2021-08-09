dfaf7935d15a3f9474025b8c363e58bd8fc91961
Kirill Lavrov <lkv@myttk.ru>
2021-08-09 17:52:27 +0700

    feat (script.js): Main task is completed

--- script.js
+++ script.js
@@ -1,1 +1,53 @@
 'use strict';
+const
+  launchTheGame = function() {
+    const
+// Границы загадываемых чисел
+    min = 1,
+    max = 100,
+// Генератор псевдослучайного число в заданных границах
+    getRandomIntInclusive = function() {
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+    },
+// Псевдослучайное число которое предстоит угадать
+    hiddenNumber = getRandomIntInclusive(),
+// Функция запроса ввода значений у пользователя
+    enterTheNumber = function(lastTry) {
+      let userResponse;
+      switch (true) {
+        case lastTry === undefined:
+          userResponse = prompt(`Угадай число от ${min} до ${max}`); break;
+        case isNaN(lastTry) || (+lastTry < min || +lastTry > max):
+          userResponse = prompt(`Введи число! (от ${min} до ${max})`); break;
+        case lastTry < hiddenNumber: 
+          userResponse = prompt('Загаданное число больше'); break;
+        case lastTry > hiddenNumber: 
+          userResponse = prompt('Загаданное число меньше'); break;
+      }
+      switch (true) {
+        case userResponse === null:
+          return false;
+        case isNaN(userResponse) || (+userResponse < 1 || +userResponse > 100): 
+          return enterTheNumber(userResponse);
+        default:
+          return userResponse;
+      } 
+    },
+//  Функция хода в игре
+    guessTheNumber = function(lastTry) {
+      let userNumber = enterTheNumber(lastTry);
+
+      if (userNumber) {
+        if (hiddenNumber === +userNumber) {
+          alert('Поздравляю, Вы угадали!!!');
+        } else {
+          guessTheNumber(userNumber);
+        }
+      } else {
+        alert('Игра окончена');
+      }
+    };
+    guessTheNumber();
+  };
+
+launchTheGame();
